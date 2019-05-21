c9f4f29b8f7780e9ac00414df2274edd6b4f88e3
Nia Bert <nbert14@gmail.com>
2019-05-21 01:08:27 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -16,13 +16,13 @@ def sort_array_char_count(array)
   end
 end
 
-def swap_element(array)
+def swap_elements(array)
   array[1], array[2] = array[2], array[1]
   array
 end
 
 def reverse_array(array)
-  array.reverse_array
+  array.reverse
 end
 
 def kesha_maker(array)
