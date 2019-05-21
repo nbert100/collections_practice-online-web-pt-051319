eb9e363b230a1720b046ffe5de61394336b86b21
Nia Bert <nbert14@gmail.com>
2019-05-21 01:05:37 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -5,3 +5,32 @@ end
 def sort_array_desc(array)
   array.sort {|a,b| b <=> a}
 end
+
+# def sort_array_desc(array)
+# array.sort.reverse
+#end
+
+def sort_array_char_count(array)
+  array.sort do |left, right|
+    left.length <=> right.length
+  end
+end
+
+def swap_element(array)
+  array[1], array[2] = array[2], array[1]
+  array
+end
+
+def reverse_array(array)
+  array.reverse_array
+end
+
+def kesha_maker(array)
+  array.each do |item|
+    item[2] = $
+  end
+end
+
+def find_a(array)
+  array.select {|string| string.start_with?("a")}
+end
